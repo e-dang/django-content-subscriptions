16ba6672ccb9370e454da7b04d003fd29fb4d50a
Eric Dang <edang830@gmail.com>
2020-08-27 16:43:53 -0700

    Add test settings

---
+++ subscriptions/tests/settings.py
@@ -0,0 +1,45 @@
+"""
+Based off of https://github.com/Fantomas42/django-tagging/blob/develop/tagging/tests/settings.py
+"""
+
+import os
+
+SECRET_KEY = 'my-super-secret-key'
+
+DATABASES = {
+    'default': {
+        'NAME': 'subscriptions.db',
+        'ENGINE': 'django.db.backends.sqlite3'
+    }
+}
+
+DATABASE_ENGINE = os.environ.get('DATABASE_ENGINE')
+if DATABASE_ENGINE == 'postgres':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'subscriptions',
+            'USER': 'postgres',
+            'HOST': 'localhost'
+        }
+    }
+elif DATABASE_ENGINE == 'mysql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'subscriptions',
+            'USER': 'root',
+            'HOST': 'localhost',
+            'TEST': {
+                'COLLATION': 'utf8_general_ci'
+            }
+        }
+    }
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.contenttypes',
+    'subscriptions',
+    'subscriptions.tests',
+]
