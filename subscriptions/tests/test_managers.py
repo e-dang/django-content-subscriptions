c3262862389d1ef5caa9453bfe9609b9808b8636
Eric Dang <edang830@gmail.com>
2020-09-01 15:04:52 -0700

    Add tests for SubscriptionManager

--- subscriptions/tests/test_managers.py
+++ subscriptions/tests/test_managers.py
@@ -1,6 +1,9 @@
 import pytest
+import mock
 
+from django.contrib.contenttypes.models import ContentType
 from .conftest import to_list
+from ..managers import SubscriptionManager
 
 
 @pytest.mark.parametrize('loaded_db, func, user_idx, expected_items_idxs', [
@@ -38,3 +41,41 @@ def test_subscribable_manager(loaded_db, func, user_idx, expected_items_idxs):
     ret_items = to_list(getattr(users[user_idx].items, func)())
 
     assert to_list(items[idx] for idx in expected_items_idxs) == ret_items
+
+
+@pytest.mark.parametrize('field, expected', [
+    ('provider', 'subscriber'),
+    ('subscriber', 'provider')
+],
+    ids=['provider', 'subscriber'])
+@pytest.mark.django_db
+def test_subscription_manager_get_opposite_field(field, expected):
+    mock_manager = mock.MagicMock(spec=SubscriptionManager)
+
+    ret_val = SubscriptionManager._get_opposite_field(mock_manager, field)
+
+    assert ret_val == expected
+
+
+@pytest.mark.parametrize('loaded_db, related_field, user_idx, expected', [
+    (None, 'subscriptions', 0, {2: ['item']}),
+    (None, 'subscribers', 0, {1: ['item'], 2: ['item']}),
+    (None, 'subscriptions', 1, {0: ['item'], 2:['item']}),
+    (None, 'subscribers', 1, {}),
+    (None, 'subscriptions', 2, {0: ['item']}),
+    (None, 'subscribers', 2, {0: ['item'], 1:['item']}),
+], indirect=['loaded_db'],
+    ids=['user0_subscriptions', 'user0_subscribers',
+         'user1_subscriptions', 'user1_subscribers',
+         'user2_subscriptions', 'user2_subscribers'])
+@pytest.mark.django_db
+def test_subscription_manager_content(loaded_db, related_field, user_idx, expected):
+    users, _ = loaded_db
+
+    ret_val = getattr(users[user_idx], related_field).content()
+
+    assert len(ret_val) == len(expected)
+    for expected_user_idx, content_types in expected.items():
+        assert users[expected_user_idx] in ret_val
+        assert to_list(ContentType.objects.get(model=ctype)
+                       for ctype in content_types) == to_list(ret_val[users[expected_user_idx]])
